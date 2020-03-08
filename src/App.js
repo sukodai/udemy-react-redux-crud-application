6100d4286c13829f9461bddfdb957d9d6d725c42
sukodai <sukodai@gmail.com>
2020-03-08 13:49:56 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 function App() {
+  //const greeting = "Hi!!";
+  //const dom = <h1 className="foo">{greeting}</h1>
   return (
-    <h1>
-      Hello, World!
-    </h1>
+    // <h1>
+    //   Hello, World!
+    // </h1>
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onClick={() =>{console.log("I am Clicked!")}} />
+    </React.Fragment>
   );
 }
 
