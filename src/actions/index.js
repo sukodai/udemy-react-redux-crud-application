01c7188788eb49f5afbfaa9b40a909dde789906e
sukodai <sukodai@gmail.com>
2020-03-14 09:52:55 +0900

    imprelemt event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,9 @@
 
 import axios from 'axios'
 
+// reducerで使えるようにexportしておく
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -16,6 +18,13 @@ export const readEvents = () => async dispatch => {
   // reducerにresponseを渡す
   dispatch({type: READ_EVENTS, response})
 }
+
+export const postEvent = values => async dispatch => {
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  dispatch({type: CREATE_EVENT, response})
+}
+
+
 
 
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,9 @@
 */
 
 import {combineReducers} from 'redux'
-//import count from './count'
+import {reducer as form} from 'redux-form'
 import events from './events'
 
-
-//export default combineReducers({count})
-export default combineReducers({events})
-
 // 複数の状態を管理できる
-//export default combineReducers({foo, bar, baz})
+export default combineReducers({events, form})
+
